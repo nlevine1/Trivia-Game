01bee328b3a217ffc2a4032895a8b16a444ce7b7
Nigel Levine <nigellevine@Nigels-MacBook-Pro.local>
2018-06-12 18:31:23 -0700

    Recent edits

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,47 +1,98 @@
-let questions = [
-    {
-        question: "Which nation won the 2014 FIFA World Cup?",
-        answerChoices: ['Brazil','Germany','Spain','Netherlands'],
-        answer: 'Germany',
-    },{
-        question: 'Which nation has won the most world cups?',
-        answerChoices: ['Germany','Itay','France','Brazil'],
-        answer: 'Brazil',
-    },{
-        question: 'Who has scored the most world cup goals?',
-        answerChoices: ['Miroslav klose','Ronaldo','Ronaldinho','Landon Donovan'],
-        answer: 'Miroslav Klose',
+// create array with question data 
+$(document).ready(function () {
+    let myQuestions = [
+        {
+            question: 'Which country won the 2014 FIFA World Cup?',
+            choices: ['Brazil', 'Germany', 'Spain', 'Netherlands'],
+            answer: 'Germany',
+        }, {
+            question: 'Which nation has won the most world cups?',
+            choices: ['Germany', 'Itay', 'France', 'Brazil'],
+            answer: 'Brazil',
+        }, {
+            question: 'Who has scored the most world cup goals?',
+            choices: ['Miroslav klose', 'Ronaldo', 'Ronaldinho', 'Landon Donovan'],
+            answer: 'Miroslav Klose',
+        }
+    ];
+
+    let userPick;
+    let rightAnswer = 0;
+    let wrongAnswer = 0;
+    let leftBlank = 0;
+    let question = 0;
+    let count = 15;
+
+    $('#start').on('click', function () {
+        $(this).hide();
+        counter = setInterval(timer, 1000);
+        displayGame();
+
+    })
+
+    function timer() {
+        count--;
+        if (count <= 0) {
+            clearInterval(counter);
+            return;
+        }
+        $('#timer').html('Time remaining: ' + count + ' secs');
     }
-];
 
-// global variables
-let currentQuestion;
-let correctAnswer;
-let wrongAnswer;
-let leftBlank;
-let seconds;
-let time;
-let answered;
-let userChoice;
-let messages = {
-    correct: 'Waahoooo!',
-    wrong: 'Wrong Wrong Wrong!',
-    timeOut: 'You are out of time!',
-    finished: 'You did it!, score time.'
-}
+    function displayGame() {
+        $('#question').html(myQuestions[0].question);
+        question++;
 
-$(document).ready(function(){
+        let choicesArr = myQuestions[0].choices;
+        // let buttonArr = [];
 
-    $('#start').on('click', function(){
-        $(this).hide();
-        // newGame();
+        for (let i = 0; i < choicesArr.length; i++) {
+            let answerList = $('<div>');
+            answerList.addClass('answerbutton');
+            answerList.text(choicesArr[i]);
+            // answerList.attr('data-id', i);
+            $('#answer').append(answerList);
+        }
+
+    }
+
+    $('.answerbutton').on('click',function() {
+        // userPick = $(this).data('id');
+        // myQuestions[0].answer;
+        console.log('test');
+        if (userPick != myQuestions[0].answer) {
+            $('#answer').text('NO! NO! NO! That/s Wrong!');
+            wrongAnswer++;
+
+        } else if (userPick === myQuestions[0].answer) {
+            $('#answer').text('Nice Work!');
+            rightAnswer++;
+        }
     });
-
-    $('#new-game').on('click', function(){
-        $(this).hide();
-        // newGame();
-    });
-
 });
+
+
+// start button and restart game button 
+
+// game functions 
+
+
+// next question function 
+
+
+	//sets up new questions & answerList
+
+	//clicking an answer will pause the time and setup answerPage
+
+// timer countdown function 
+
+
+// show time countdown function 
+
+
+// answerpage function that tracks right, wrong and not answered counters 
+// display results function 
+
+
 
 
